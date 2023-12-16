a3e7782b95614a18e0a411d8ad69f10faa03e7e7
razzberrygarderob23 <153976849+razzberrygarderob23@users.noreply.github.com>
2023-12-16 13:44:03 +0400

    model migration

---
+++ database/migrations/2023_12_16_091946_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->float('price');
+            $table->float('size');
+            $table->string('materials');
+            $table->string('country');
+            $table->string('color');
+            $table->string('image');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
